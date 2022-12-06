5e2dfa72cc0d569fa05862abcc60391e0dd78418
Christopher <leyssen@gmail.com>
2022-12-06 08:31:43 +0100

    day 6

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,41 +1,54 @@
-fn parse_(input: &str) -> impl Iterator<Item = &str> + '_ {
-    input.trim().lines()
+use std::collections::HashSet;
+
+fn find_marker(input: &str, start_marker_size: usize) -> usize {
+    input
+        .as_bytes()
+        .windows(start_marker_size)
+        .map(|window| window.iter().copied().collect::<HashSet<_>>().len())
+        .position(|len| len == start_marker_size)
+        .unwrap()
+        + start_marker_size
 }
 
 #[must_use]
 pub fn part_1(input: &str) -> usize {
-    parse_(input).count()
+    find_marker(input, 4)
 }
 
 #[must_use]
 pub fn part_2(input: &str) -> usize {
-    parse_(input).count()
+    find_marker(input, 14)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = include_str!("../example.txt");
     const INPUT: &str = include_str!("../input.txt");
 
     #[test]
-    fn part_1_example() {
-        assert_eq!(0, part_1(EXAMPLE));
+    fn part_1_examples() {
+        assert_eq!(5, part_1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(6, part_1("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(10, part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(11, part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
     }
 
     #[test]
     fn part_1_input() {
-        assert_eq!(0, part_1(INPUT));
+        assert_eq!(1702, part_1(INPUT));
     }
 
     #[test]
-    fn part_2_example() {
-        assert_eq!(0, part_2(EXAMPLE));
+    fn part_2_examples() {
+        assert_eq!(5, part_1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(6, part_1("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(10, part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(11, part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
     }
 
     #[test]
     fn part_2_input() {
-        assert_eq!(0, part_2(INPUT));
+        assert_eq!(3559, part_2(INPUT));
     }
 }
